7ecc49ff94e3e6b122e74b6ad018796374a5fa9c
lorenagayoso <lorena.gayoso.g@gmail.com>
2021-12-05 14:49:31 -0300

    cart widget

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Navigation from './components/NavBar.js'
+import ItemListContainer from './components/ItemListContainer.js'
 
 function App() {
   return (
   <React.Fragment className="App">
-  <Navigation/>
+  <Navigation cat1="Men" cat2="Women" cat3="Children"/>
+  <ItemListContainer/>
   </React.Fragment>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,36 @@
-import logo from '../logo.svg';
+import logo from '../logo.svg'
+import '../App.css';
+import cart from './cart_icon.png'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import { Nav , Navbar , NavDropdown } from 'react-bootstrap'
 
-function Navigation() {
+function Navigation(props) {
+  const {cat1:category_1, cat2:category_2 , cat3: category_3} = props
   return (
-    <div className="App">
+    <div className="navClass">
+
     <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
     <Navbar.Brand>
     <img src={logo} width="40px" height="40px" />
-    MyLogo
+    Ecommerce
     </Navbar.Brand>
     <Navbar.Toggle/>
     <Navbar.Collapse>
     <Nav>
     <Nav.Link href="about-us">About us</Nav.Link>
     <NavDropdown title="categories">
-    <NavDropdown.Item href="categories/men">Men</NavDropdown.Item>
-    <NavDropdown.Item href="categories/women">Women</NavDropdown.Item>
-    <NavDropdown.Item href="categories/children">Children</NavDropdown.Item>
+    <NavDropdown.Item href="categories/men">{category_1}</NavDropdown.Item>
+    <NavDropdown.Item href="categories/women">{category_2}</NavDropdown.Item>
+    <NavDropdown.Item href="categories/children">{category_3}</NavDropdown.Item>
     </NavDropdown>
+    <button class="btn btn-outline-info my-2 my-sm-0">Promos</button>
     <Nav.Link href="contact">Contact</Nav.Link>
     </Nav>
     </Navbar.Collapse>
+    <div id="cart">
+    <img src={cart} width="40px" height="40px"/>
+    </div>
     </Navbar>
     </div>
   );
